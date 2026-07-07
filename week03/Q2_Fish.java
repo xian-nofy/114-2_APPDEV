b0cca4cacb76626bd7b9b85a9b380dd3558aa1f1
xian-nofy <c112181101@nkust.edu.tw>
2026-07-07 17:34:16 +0800

    完成第三週作業：海洋類別、魚、海龜

--- week03/Q2_Fish.java
+++ week03/Q2_Fish.java
@@ -1,18 +1,23 @@
-public class Q2_fish{
+public class Q2_Fish {
+    // 1. 宣告屬性
     String name;
     double weight;
 
-    public void displayinf(){
-         System.out.println("This fish's name is:" + name + "Weight: "+ wieght +"KG");
+    // 2. 撰寫行為方法
+    public void displayInfo() {
+        System.out.println("這隻魚的名字是：" + name + "，重量是：" + weight + "公斤");
     }
 
-    public static void main(String [] args){
-        Q2_fish myfish = new Q2_fish();
-        myfish.name = "Black fish";
-        myfish.weight =250.5;
+    // 3. 主程式執行區
+    public static void main(String[] args) {
+        // 產生一個 Q2_Fish 的物件
+        Q2_Fish myFish = new Q2_Fish();
 
-        myfish.displayinf();
+        // 設定這隻魚的屬性值
+        myFish.name = "黑鮪魚";
+        myFish.weight = 250.5;
 
+        // 呼叫這隻魚的方法來印出資訊
+        myFish.displayInfo();
     }
-
 }
--- week03/Q3_Turtle.java
+++ week03/Q3_Turtle.java
@@ -1,20 +1,25 @@
-public class Q3_Turtle{
+public class Q3_Turtle {
+    // 1. 已經宣告好的屬性
     String species;
     int age;
 
-    public Q3_Turtle(String species, int age){
-        this.species = species;
-        this.age = age;
+    // 2. 撰寫建構子 (Constructor)
+    public Q3_Turtle(String species, int age) {
+        this.species = species; // 將傳入的參數值指定給類別的屬性
+        this.age = age;         // 將傳入的參數值指定給類別的屬性
     }
 
-    public void showDetail(){
-         System.out.println("This turtle's species is:" + species + "age: "+ age);
+    // 印出資訊的方法
+    public void showDetails() {
+        System.out.println("品種：" + species + "，年紀：" + age + "歲");
     }
 
-    public static void main(String [] args){
-      Q3_Turtle myTurtle = new Q3_Turtle("Green Turtle", 50);
-      myTurtle.showDetail();
+    // 3. 主程式執行區
+    public static void main(String[] args) {
+        // 利用建構子，在建立物件的當下直接傳入 "綠蠵龜" 與 50
+        Q3_Turtle myTurtle = new Q3_Turtle("綠蠵龜", 50);
 
+        // 呼叫方法印出資訊
+        myTurtle.showDetails();
     }
-
 }
